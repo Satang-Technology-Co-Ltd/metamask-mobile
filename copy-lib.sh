b89053618052a16f2e203d3ac960217d404038a4
Ic3Sandy <monkey_king2@hotmail.com>
2021-12-15 14:09:42 +0700

    🚧 Update for custom lib

---
+++ copy-lib.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+echo "node_modules/ethereumjs-wallet/dist/index.js:401"
+cp custom-lib/ethereumjs-wallet.js node_modules/ethereumjs-wallet/dist/index.js
+
+echo "node_modules/@metamask/controllers/dist/keyring/KeyringController.js:238"
+cp custom-lib/KeyringController.js node_modules/@metamask/controllers/dist/keyring/KeyringController.js
+
+echo "node_modules/@evercode-lab/qtumcore-lib/index.js:24"
+cp custom-lib/qtumcore-lib.js node_modules/@evercode-lab/qtumcore-lib/index.js 
+
+echo "node_modules/@metamask/controllers/dist/transaction/TransactionController.js:318"
+cp custom-lib/TransactionController.js node_modules/@metamask/controllers/dist/transaction/TransactionController.js
