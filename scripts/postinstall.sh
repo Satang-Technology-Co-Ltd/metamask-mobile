10269133e4f93874c4fb7fb45e72e1ea6a5b9cdd
Ic3Sandy <monkey_king2@hotmail.com>
2022-01-26 17:14:33 +0700

    🚧 Update script postinstall

--- scripts/postinstall.sh
+++ scripts/postinstall.sh
@@ -11,9 +11,18 @@ echo "3. Patch npm packages"
 npx patch-package
 
 echo "4. Create xcconfig files..."
-echo "" > ios/debug.xcconfig
-echo "" > ios/release.xcconfig
+echo "" >ios/debug.xcconfig
+echo "" >ios/release.xcconfig
 
 echo "5. Init git submodules"
 echo "This may take a while..."
 git submodule update --init
+
+if [ ! -d "ios/branch-ios-sdk" ]; then
+    git clone -b 1.40.2 https://github.com/BranchMetrics/ios-branch-deep-linking-attribution ios/branch-ios-sdk
+    echo "Git clone ios/branch-ios-sdk"
+fi
+if [ ! -d "ios/mixpanel-iphone" ]; then
+    git clone -b v3.9.2 https://github.com/mixpanel/mixpanel-iphone ios/mixpanel-iphone
+    echo "Git clone ios/mixpanel-iphone"
+fi
